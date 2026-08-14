a04e0675e0add13487809120fc8b50a25b3bbd70
boufeldjaabdeldjalil2023-star <boufeldja.abdeldjalil2023@gmail.com>
2026-08-14 17:14:41 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,6 +4,11 @@ const commands = [
   new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!')
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Shows the bot commands')
     .toJSON()
 ];
 
@@ -11,14 +16,14 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log('Registering /ping...');
+    console.log('Registering commands...');
 
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
 
-    console.log('/ping registered successfully!');
+    console.log('Commands registered successfully!');
   } catch (error) {
     console.error(error);
   }
